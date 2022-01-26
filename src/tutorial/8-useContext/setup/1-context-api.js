f7e80c268411c79daf1595cc47e6e3329bd01545
kareemah <ajimobikareemah@gmail.com>
2022-01-26 15:36:46 +0100

    useContext/context API tutorial

--- src/tutorial/8-useContext/setup/1-context-api.js
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -3,7 +3,10 @@ import { data } from '../../../data';
 // more components
 // fix - context api, redux (for more complex cases)
 
+const personContext = React.createContext();
 const ContextAPI = () => {
+  // now we have access to two hooks, the provider and the consumer
+  // provider works as  a distributor
   const [people, setPeople] = useState(data);
   const removePerson = (id) => {
     setPeople((people) => {
@@ -11,32 +14,28 @@ const ContextAPI = () => {
     });
   };
   return (
-    <>
-      <h3>prop drilling</h3>
-      <List people={people} removePerson={removePerson} />
-    </>
+    <personContext.Provider value={{ removePerson, people }}>
+      <h3>Context API/useContext</h3>
+      <List />
+    </personContext.Provider>
   );
 };
 
-const List = ({ people, removePerson }) => {
+const { people } = useContext(personContext);
+const List = () => {
   return (
     <>
       {people.map((person) => {
-        return (
-          <SinglePerson
-            key={person.id}
-            {...person}
-            removePerson={removePerson}
-          />
-        );
+        return <SinglePerson key={person.id} {...person} />;
       })}
     </>
   );
 };
 
-const SinglePerson = ({ id, name, removePerson }) => {
+const SinglePerson = ({ id, name }) => {
+  const { removePerson } = useContext(personContext);
   return (
-    <div className='item'>
+    <div className="item">
       <h4>{name}</h4>
       <button onClick={() => removePerson(id)}>remove</button>
     </div>
